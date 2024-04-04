f36235c188e2e0c6180803fe5629d65454162556
MercyAkpan <akpanmeme@gmail.com>
2024-04-04 23:28:44 +0100

    todo

--- models/todo_model.py
+++ models/todo_model.py
@@ -3,7 +3,6 @@ from uuid import uuid4
 
 
 class Todo:
-
     def __init__(self, *args, **kwargs) -> None:
         if kwargs:
             for key in kwargs.keys():
@@ -15,10 +14,10 @@ class Todo:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            self.task = {}
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
+        
     def save(self):
         """ update the current datetime """
         from models import storage
@@ -27,12 +26,17 @@ class Todo:
         storage.new(self)
         storage.save()
 
-    def create_todo(self, todo_name, text):
-        self.todo_name = todo_name
-        self.text = text
-
+    def create_task(self, task_title, task_description):
+        """ """
+        self.task[task_title] = task_description # No need to put self, to parameters, as they are outside vars
+        return
+    def create_todo(self, todo_name = "Todo", text = "I need this done"):
+        #self.__dict__[Todo.task_number] = {todo_name: text}
+        # self.text = text
+        pass
     def to_dict(self):
         object = self.__dict__.copy()
         object["created_at"] = object["created_at"].isoformat()
         object["updated_at"] = object["updated_at"].isoformat()
+        #object[Todo.task_number] = self.create_todo() 
         return object
--- text.py
+++ text.py
@@ -1,5 +1,6 @@
 from models.todo_model import Todo
 new = Todo()
-new.create_todo("name", "Texted")
+# new.create_todo("name", "Texted")
 new.to_dict()
+new.create_task("EATING", "BY 9 AM")
 new.save()
